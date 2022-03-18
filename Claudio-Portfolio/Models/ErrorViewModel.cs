60e0c36ae7bf16c5cf12c5be036b56b7718be5e7
Terogene Claudio <cterogene@gmail.com>
2022-03-18 11:14:11 -0800

    Add project files.

---
+++ Claudio-Portfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Claudio_Portfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
